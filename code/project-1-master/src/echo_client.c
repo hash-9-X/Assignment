1960f029192bbdca90696b86ab898f22cc35741e
jiuaosong <jiuaosong@gmail.com>
2021-10-03 20:48:01 +0800

    第二次更改，大致实现功能

--- code/project-1-master/src/echo_client.c
+++ code/project-1-master/src/echo_client.c
@@ -18,59 +18,69 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 #include <netinet/ip.h>
 
 #define ECHO_PORT 9999
 #define BUF_SIZE 4096
+
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+
+    if (argc != 4)
     {
-        fprintf(stderr, "usage: %s <server-ip> <port>",argv[0]);
+        fprintf(stderr, "usage: %s <server-ip> <port> <msgFile>", argv[0]);
         return EXIT_FAILURE;
     }
 
-    char buf[BUF_SIZE];//?
-      
+    char buf[BUF_SIZE];
+
     int status, sock;
     struct addrinfo hints;
 	memset(&hints, 0, sizeof(struct addrinfo));
-    struct addrinfo *servinfo; //will point to the results
-    hints.ai_family = AF_INET;  //IPv4
-    hints.ai_socktype = SOCK_STREAM; //TCP stream sockets
-    hints.ai_flags = AI_PASSIVE; //fill in my IP for me
+    struct addrinfo *servinfo;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-    if ((status = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) 
+    if ((status = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0)
     {
         fprintf(stderr, "getaddrinfo error: %s \n", gai_strerror(status));
         return EXIT_FAILURE;
     }
+
 
     if((sock = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
     {
         fprintf(stderr, "Socket failed");
         return EXIT_FAILURE;
     }
-    
+
     if (connect (sock, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
     {
         fprintf(stderr, "Connect");
         return EXIT_FAILURE;
     }
-        
-    char msg[BUF_SIZE]; 
-    fgets(msg, BUF_SIZE, stdin);
-    
+
+    char msg[BUF_SIZE];
+    // fgets(msg, BUF_SIZE, stdin);
+    int fd_in = open(argv[3], O_RDONLY);
+	if(fd_in < 0) {
+	    printf("Failed to open the file\n");
+		return 0;
+    }
+    read(fd_in, msg, BUF_SIZE);
+
     int bytes_received;
-    fprintf(stdout, "Sending %s", msg);
+    //fprintf(stdout, "Sending %s", msg);
     send(sock, msg , strlen(msg), 0);
     if((bytes_received = recv(sock, buf, BUF_SIZE, 0)) > 1)
     {
         buf[bytes_received] = '\0';
         fprintf(stdout, "Received %s", buf);
-    }        
+    }
 
     freeaddrinfo(servinfo);
-    close(sock);    
+    close(sock);
     return EXIT_SUCCESS;
 }
